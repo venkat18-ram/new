5f3a4a033b1f0b4ea3f625af908de8ae8b939377
venkat18-ram <battikerivenkatram2006@gmail.com>
2026-04-29 13:44:00 +0530

    just detail

--- save.py
+++ save.py
@@ -3,10 +3,9 @@ data={
     'reg id':['24FH1A05F5','24FH1A05H3','24FH1A05I8','24FH1A05J6','24FH1A05H8','24FH1A05G8','24FH1A05J6','24FH1A05G1','24FH1A05F7','24FH1A05F3','24FH1A05H0','24FH1A05I9','24FH1A05F4','24FH1A05F6'],
     'Name':['venkat','suhail','jahash','shiva','bansi','Vardhan','khaiser','mahesh','sethu sai','sai teja','affan','junaid','govardhan','kishore'],
     'age':[20,20,20,20,20,20,20,20,20,20,20,20,20,20],
-    'attendence':[85,84,90,78,75,88,70,58,60,74,78,74,68,55]
 }
 df=pd.DataFrame(data)
 print(df)
-df.to_csv('new/CSE-C details.csv',index=False)
+# df.to_csv('new/CSE-C details.csv',index=False)
 # df.to_json('new/CSE-Cdetails.json',index=True)
-# df.to_excel('new/CSE-Cdetails.xlsx',index=False)
+df.to_excel('new/CSE-Cdetails.xlsx',index=False)
